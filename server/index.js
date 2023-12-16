370e858a136a173477f892e7fe5c5c68214b9627
Gimletdmdm <business.dmdm@gmail.com>
2023-12-16 23:11:08 +0900

    hennkou

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const app = express()
 app.use('/api/v1/products', productRouter);
 
 if(process.env.NODE_ENV === 'production') {
-    const appPath = path.join(__dirname, '..', 'dist', 'udemy-angular');
+    const appPath = path.join(__dirname, 'dist', 'udemy-angular');
     app.use(express.static(appPath));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(appPath, 'index.html'));
