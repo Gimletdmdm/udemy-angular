b42cee579e190c1521755f80245cda877a8ae430
Gimletdmdm <business.dmdm@gmail.com>
2023-12-18 20:44:06 +0900

    environment add

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, 
   {
