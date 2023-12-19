38eec2952879ff88ee4a25ebfc613b4ecce429b6
Gimletdmdm <business.dmdm@gmail.com>
2023-12-19 21:56:17 +0900

    prod

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,4 +1,3 @@
 module.exports = {
-    DB_URI: 'mongodb+srv://udemy-anguler:Business.dmdm1@cluster0.eswpjd5.mongodb.net/?retryWrites=true&w=majority'
-    //DB_URI: process.env.DB_URI
+    DB_URI: process.env.DB_URI
 }
