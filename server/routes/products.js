226d79e29151cdddae431352777a3dba24cc6bd5
Gimletdmdm <business.dmdm@gmail.com>
2023-12-21 22:49:45 +0900

    async 変更

--- server/routes/products.js
+++ server/routes/products.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const Product = require('../model/product')
 
 // find()はコールバックを引数に取れなくなったので注意⇒非同期処理で対応する
-router.get('', async (req, res) => {
+router.get('', (req, res) => {
     try {
-        foundProducts = await Product.find({});
+        foundProducts = Product.find({});
         return res.json(foundProducts);
     } catch(err) {
         res.status(422).send({error: [{title: 'product error', detail: 'Product not found'}]})
@@ -14,10 +14,10 @@ router.get('', async (req, res) => {
     
 });
 
-router.get('/:productId', async (req, res) => {
+router.get('/:productId', (req, res) => {
     const productId = req.params.productId;
     try {
-        foundProduct = await Product.findById(productId);
+        foundProduct = Product.findById(productId);
         return res.json(foundProduct);
     } catch(err) {
         res.status(422).send({error: [{title: 'ProductId error', detail: 'ProductId not found!'}]});
